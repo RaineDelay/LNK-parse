fe0046f6215e474223095cf78db085994729fc83
RaineDelay <jtroubetaris475@gmail.com>
2023-05-14 05:31:35 -0400

    updated main and parser to work together and resolved a type error with the filepath

--- lnkparse/__main__.py
+++ lnkparse/__main__.py
@@ -1,9 +1,12 @@
 import argparse
+from parser import *
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath", type=str, help="Parse the specified .lnk file or .lnk files within a directory")
     args = parser.parse_args()
+    extract(args.filepath)
+
 
 if __name__ == "__main__":
     main()
--- lnkparse/parser.py
+++ lnkparse/parser.py
@@ -1,15 +1,16 @@
-
 # I'm planning on adding the print statements after, not sure if I should do that as the file is parsed or if I should save 
 # the results and format the printing later.
+results = {}
 
-def extract(fd):
-    with open(fd, "rb"):
+def extract(filepath):
+    with open(filepath, "rb") as fd:
         header_size = int.from_bytes(fd.read(4), "little")
+        print(header_size)
         results["header_size"] = header_size
         CLSID = fd.read(16)                                 # write the check and formatting later probably save as a string
         set_flags = get_flags(fd.read(4))                   # flags in this list are true, any other flag is inherently false
-
-
+        set_attrs = get_attributes(fd.read(4))              # same as flags but with the file's attributes instead
+        
 
 
 def get_flags(byte_arr):
@@ -23,7 +24,9 @@ def get_flags(byte_arr):
                  "DisableLinkPathTracking", "DisableKnownFolderTracking", "DisableKnownFolderAlias",
                  "AllowLinkToLink", "UnaliasOnSave", "PreferEnivronmentPath", "KeepLocalIDListForUNCTarget"
                 ]
-   set_flags = [flag for i, flag in enumerate(flag_list) if (flag_list[i // 8] & (1 << (7 - (i % 8)))) > 0]
-   return set_flags
+    set_flags = [flag for i, flag in enumerate(flag_list) if (byte_arr[i // 8] & (1 << (7 - (i % 8)))) > 0]
+    print(set_flags)
+    return set_flags
     
-
+def get_attributes(byte_arr):
+    print("hello attributes")
