4f5d7613a0918eaaa4ec78a5d0b0ec093c37d139
RaineDelay <jtroubetaris475@gmail.com>
2023-05-16 20:40:25 -0400

    added a lot, not much time to explain

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # LNK-parse
 A small CLI tool for parsing the metadata from .lnk files
+
+The tool currently takes a single .lnk file's local path as input and
+I have not made this its own package yet so currently it's called by
+doing "Python3 __main__.py " plus the filepath
+
+It's currently only capable of parsing the header size, link flags,
+link attributes, and the creation/access/write times
+
+For testing I used a .lnk for Internet Explorer I got from a windows VM.
+To actually test that the results were correct, I manually calculated
+what the values of everything should be based of the hexdump output
+of its bytes.
--- lnkparse/parser.py
+++ lnkparse/parser.py
@@ -1,7 +1,12 @@
-# I'm planning on adding the print statements after, not sure if I should do that as
-# the file is parsed or if I should save the results and format the printing later.
+import struct
+from datetime import datetime
 
+utc_start = datetime.fromisoformat("1970-01-01")
 results = {}
+
+# extract(filepath) is the primary function which calls every other individual function.
+# it handles the file IO and simply prints what the other functions calculate
+# along with adding those calcs to the results dictionary
 
 
 def extract(filepath):
@@ -12,12 +17,25 @@ def extract(filepath):
         CLSID = fd.read(
             16
         )  # write the check and formatting later probably save as a string
+        print(CLSID)
         set_flags = get_flags(
             fd.read(4)
         )  # flags in this list are true, any other flag is inherently false
+        print(set_flags)
         set_attrs = get_attributes(
             fd.read(4)
         )  # same as flags but with the file's attributes instead
+        print(set_attrs)
+        results["Creation Time"] = time_calc(fd.read(8))
+        print("Creation Time: {}".format(results["Creation Time"]))
+        results["Access Time"] = time_calc(fd.read(8))
+        print("Access Time: {}".format(results["Access Time"]))
+        results["Write Time"] = time_calc(fd.read(8))
+        print("Write Time: {}".format(results["Write Time"]))
+
+
+# get_flags takes a byte array of exactly four bytes, then performs a bitmask over these
+# bytes to determine which are set.
 
 
 def get_flags(byte_arr):
@@ -56,11 +74,54 @@ def get_flags(byte_arr):
     set_flags = [
         flag
         for i, flag in enumerate(flag_list)
-        if (byte_arr[i // 8] & (1 << (7 - (i % 8)))) > 0
+        if (byte_arr[i // 8] & (1 << (i % 8))) > 0
     ]
-    print(set_flags)
     return set_flags
 
 
+# get_attributes is the same process as get_flags but with a different list
+
+
 def get_attributes(byte_arr):
-    print("hello attributes")
+    attr_list = [
+        "FILE_ATTRIBUTE_READONLY",
+        "FILE_ATTRIBUTE_HIDDEN",
+        "FILE_ATTRIBUTE_SYSTEM",
+        "Reserved1",
+        "FILE_ATTRIBUTE_DIRECTORY",
+        "FILE_ATTRIBUTE_ARCHIVE",
+        "Reserved2",
+        "FILE_ATTRIBUTE_NORMAL",
+        "FILE_ATTRIBUTE_TEMPORARY",
+        "FILE_ATTRIBUTE_SPARSE_FILE",
+        "FILE_ATTRIBUTE_REPARSE_POINT",
+        "FILE_ATTRIBUTE_COMPRESSED",
+        "FILE_ATTRIBUTE_OFFLINE",
+        "FILE_ATTRIBUTE_NOT_CONTENT_INDEXED",
+        "FILE_ATTRIBUTE_ENCRYPTED",
+    ]
+    set_attrs = [
+        attr
+        for i, attr in enumerate(attr_list)
+        if (byte_arr[i // 8] & (1 << (i % 8))) > 0
+    ]
+    return set_attrs
+
+
+# time_calc takes a byte array of size 8, unpacks to a long long, then subtract
+# the difference between the Windows epoch and the Linux epoch
+
+
+def time_calc(byte_arr):
+    micro = struct.unpack("<q", byte_arr)[0]
+    result = datetime.fromtimestamp(micro / 10000000.0 - 11644473600).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+    # the following was a failed attempt
+    # seconds = int.from_bytes(byte_arr, "little", signed="False") / 100000000
+    # print(micro)
+    # delta = timedelta(seconds=seconds)
+    # print(delta)
+    # print(utc_start + delta)
+    # return utc_start + delta
+    return result
